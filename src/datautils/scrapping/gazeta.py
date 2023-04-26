f09f29e54d7dda870e3e0147251eea09f4e84f35
amolina <evabot505@gmail.com>
2023-04-26 12:41:52 +0200

    MARE DE DEU CRISTO SENYOR

---
+++ src/datautils/scrapping/gazeta.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+import uuid
+import re
+import os
+
+LINKS = {
+    
+}
--- src/datautils/scrapping/scrapper-ministerio.sh
+++ src/datautils/scrapping/scrapper-ministerio.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-while read i
-do
-  wget $i -O scrapped/${i: -6}.pdf 
-done < $1
+#!/bin/bash
+
+for i in {1001481801..1001639776}; do
+    link="https://prensahistorica.mcu.es/es/catalogo_imagenes/grupo.do?path=$i"
+    pdfname="$i.pdf"
+    wget -q --no-check-certificate --output-document=scrapped/$pdfname $link
+done
