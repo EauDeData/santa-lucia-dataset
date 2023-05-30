2b9f8eedf95d511a0e2fa7693ad0b0db4d4eb3be
amolina <evabot505@gmail.com>
2023-05-30 15:18:44 +0200

    ocr

--- unitary_tests.py
+++ unitary_tests.py
@@ -1,5 +1,5 @@
-from src.datautils.dataloader import ZippedDataloader
-from src.process.segmentation import segment, crop_lines, lp_detect
+from src.datautils.dataloader import ZippedDataloader, read_img
+from src.process.segmentation import segment, crop_lines, lp_detect, MODELS
 
 import argparse
 import uuid
@@ -10,25 +10,22 @@ import matplotlib.pyplot as plt
 import layoutparser as lp
 import numpy as np
 
-parser = argparse.ArgumentParser(
-                    prog='Lorem Ipsum',
-                    description='Super Trouper',
-                    epilog='Uwu')
 
-parser.add_argument('-f', '--file', default='./diaris.zip')
-parser.add_argument('-o', '--output', default='./cropped/')
-args = parser.parse_args()
-os.makedirs(args.output, exist_ok = True)
+image = read_img('samples/diari.pdf')[0]
+for model_name in MODELS:
+    model = lp.models.Detectron2LayoutModel(config_path = MODELS[model_name]['model'], label_map   = MODELS[model_name]['labels'], extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8])
+    img = lp.visualization.draw_box(image, lp_detect(image, model), color_map = {})
+    cv2.imwrite(f'results/{model_name}-lp.png', cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR))
 
-dataset = ZippedDataloader(args.file)
+model = lp.models.Detectron2LayoutModel(config_path = MODELS['hjdataset']['model'], label_map   = MODELS['hjdataset']['labels'], extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8])
+img = lp.visualization.draw_box(image.transpose(1, 0, 2), lp_detect(image.transpose(1, 0, 2), model), color_map = {})
+cv2.imwrite(f'results/hjdataset-lp.t.png', cv2.cvtColor(np.array(img).transpose(1, 0, 2), cv2.COLOR_RGB2BGR))
 
-for n, image in tqdm(enumerate(dataset,)):
-    #image = image.transpose(1, 0, 2)
-    layout = lp_detect(image[0])
-    for item in layout:
-        print(item.block)
-        print(item.type, item.coordinates, item.score)
-    vis = np.array(lp.visualization.draw_box(image[0], layout)) #.transpose(1, 0, 2)
-    plt.imshow(vis)
-    plt.show()
+image_gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+bbxs = segment(image_gs)
 
+canvas = image.copy()
+for box in bbxs:
+    if not isinstance(box, int): canvas = cv2.rectangle(canvas, box[:2], [a+w for a,w, in zip(box[2:], box[:2])], (255, 0, 0), 5)
+
+cv2.imwrite(f'results/ours.png', cv2.cvtColor(np.array(canvas), cv2.COLOR_RGB2BGR))
